81081ca84eb58bb8855a8beb17427d32706251af
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2020-08-29 18:29:17 +0300

    Add SimpleEncode/Decode

--- src/algos/algorithms.go
+++ src/algos/algorithms.go
@@ -1,29 +1,93 @@
 package algos
 
 import (
-	"fmt"
+	"strings"
 	"image"
 	"os"
+	"image/draw"
+	"image/color"
 	_ "image/jpeg"
-	_ "image/png"
+	"image/png"
 )
 
-// SimpleAlgo Super simple algorithm :thinking: 
-func SimpleAlgo(imgPath string) {
+func loadImageRGBA(imgPath string) *image.RGBA {
 	f, err := os.Open(imgPath)
 	if err != nil{
-		fmt.Println(err)
+		panic(err)
 	}
 	defer f.Close()
 
-	img, fmtName, err := image.Decode(f)
+	img, _, err := image.Decode(f)
 	if err != nil{
-		fmt.Println(err)
+		panic(err)
 	}
 
-	bounds := img.Bounds()
+	dimg, ok := img.(*image.RGBA)
+	if !ok {
+		bounds := img.Bounds()
+		dimg = image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+		draw.Draw(dimg, dimg.Bounds(), img, bounds.Min, draw.Src)
+	}
 
-	fmt.Println(fmtName)
-	fmt.Println(bounds.Max.X)
-	fmt.Println(bounds.Max.Y)
+	return dimg
+}
+
+func saveImagePNG(img *image.RGBA, savePath string) {
+	f, err := os.Create(savePath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = png.Encode(f, img)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func stringToASCIIInt(str string) []int {
+	runes := []rune(str)
+	var result []int
+	for i := 0; i < len(runes); i++ {
+		result = append(result, int(runes[i]))
+	}
+
+	return result
+}
+
+// SimpleAlgo Super simple algorithm :thinking: 
+func SimpleAlgoEncode(imgPath, saveImgPath, str string) {
+	img := loadImageRGBA(imgPath)
+	strValues := stringToASCIIInt(str)
+	strIndex := 0
+
+	for y := 0; y < img.Bounds().Max.Y; y++ {
+		for x := 0; x < img.Bounds().Max.X; x++ {
+			if strIndex < len(strValues) {
+				r,g,b,a := img.At(x, y).RGBA()
+				a = 255 - uint32(strValues[strIndex])
+				img.Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)})
+				strIndex = strIndex + 1
+			} else {
+				y = 9999999
+				x = 9999999
+			}
+		}
+	}
+	saveImagePNG(img, saveImgPath)
+}
+
+func SimpleAlgoDecode(imgPath string) string {
+	img := loadImageRGBA(imgPath)
+	var buffer strings.Builder
+
+	for y := 0; y < img.Bounds().Max.Y; y++ {
+		for x := 0; x < img.Bounds().Max.X; x++ {
+			_,_,_,a := img.At(x, y).RGBA()
+			if a>>8 == 255 { return buffer.String() }
+			buffer.WriteString(string(rune(255 - (a>>8))))
+		}
+	}
+
+	return buffer.String()
 }
--- src/main.go
+++ src/main.go
@@ -1,7 +1,25 @@
 package main
 
-import ( "gohide/algos" )
+import (
+	"gohide/algos"
+	"fmt"
+	"os"
+)
+
+func showHelp() {
+	fmt.Println("Options:\n-se [img] [output img] [ASCII string] (Simple Encode)\n-sd [img] (Simple Decode)\n")
+}
 
 func main() {
-	algos.SimpleAlgo("yes.jpg");
+	if len(os.Args) > 1 {
+		if os.Args[1] == "-se" {
+			if len(os.Args) > 4 {
+				algos.SimpleAlgoEncode(os.Args[2], os.Args[3], os.Args[4])
+			} else { showHelp() }
+		} else if os.Args[1] == "-sd" {
+			if len(os.Args) > 2 {
+				fmt.Println(algos.SimpleAlgoDecode(os.Args[2]))
+			} else { showHelp() }
+		}
+	} else { showHelp() }
 }
